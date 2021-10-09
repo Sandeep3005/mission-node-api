b523fb2d085520961eee1de06a590572b5df8d97
Sandeep Chikhale <chikhale.sandeep.2002@gmail.com>
2021-10-09 19:26:04 +0530

    Add all routes

--- source/controllers/sample.ts
+++ source/controllers/sample.ts
@@ -1,9 +1,85 @@
-import { NextFunction, Request, Response } from 'express';
+import { NextFunction, Request, Response } from "express";
 
 const serverHealthCheck = (req: Request, res: Response, next: NextFunction) => {
-    return res.status(200).json({
-        message: 'pong'
-    });
+  return res.status(200).json({
+    message: "pong",
+  });
 };
 
-export default { serverHealthCheck };
+const getAllMovies = (req: Request, res: Response, next: NextFunction) => {
+  const movies = [{ id: 1, name: "Mission: Impossible" }];
+  return res.status(200).json({ movies });
+};
+
+const getOneMovie = (req: Request, res: Response, next: NextFunction) => {
+  const movies = [{ id: "1", name: "Mission: Impossible" }];
+  let { id } = req.params;
+  const movie = movies.find((movie) => movie.id === id);
+  return res.status(200).json({ movie });
+};
+
+const getAllCastMembers = (req: Request, res: Response, next: NextFunction) => {
+  const castMembers = [
+    {
+      id: "1",
+      name: "Tom Cruise",
+      character: "Ethan Hunt",
+      appearedIn: ["Mission: Impossible", "Mission: Impossible 2"],
+    },
+    {
+      id: "2",
+      name: "Jon Voight",
+      character: "Jim Phelps",
+      appearedIn: ["Mission: Impossible"],
+    },
+  ];
+  return res.status(200).json({ castMembers });
+};
+
+const getOneCastMember = (req: Request, res: Response, next: NextFunction) => {
+  const castMembers = [
+    {
+      id: "1",
+      name: "Tom Cruise",
+      character: "Ethan Hunt",
+      appearedIn: ["Mission: Impossible", "Mission: Impossible 2"],
+    },
+    {
+      id: "2",
+      name: "Jon Voight",
+      character: "Jim Phelps",
+      appearedIn: ["Mission: Impossible"],
+    },
+  ];
+  let { name } = req.params;
+  const castMember = castMembers.find((cast) => cast.name === name);
+  return res.status(200).json({ castMember });
+};
+
+const getRandomTeam = (req: Request, res: Response, next: NextFunction) => {
+  const castMembers = [
+    {
+      id: "1",
+      name: "Tom Cruise",
+      character: "Ethan Hunt",
+      appearedIn: ["Mission: Impossible", "Mission: Impossible 2"],
+    },
+    {
+      id: "2",
+      name: "Jon Voight",
+      character: "Jim Phelps",
+      appearedIn: ["Mission: Impossible"],
+    },
+  ];
+
+  return res.status(200).json({ castMembers });
+};
+
+export default {
+  serverHealthCheck,
+  getAllMovies,
+  getOneMovie,
+  getAllCastMembers,
+  getOneCastMember,
+  getRandomTeam,
+};
--- source/routes/sample.ts
+++ source/routes/sample.ts
@@ -1,8 +1,13 @@
-import express from 'express';
-import controller from '../controllers/sample';
+import express from "express";
+import controller from "../controllers/sample";
 
 const router = express.Router();
 
-router.get('/ping', controller.serverHealthCheck);
+router.get("/ping", controller.serverHealthCheck);
+router.get("/movies", controller.getAllMovies);
+router.get("/movie/:id", controller.getOneMovie);
+router.get("/castMembers", controller.getAllCastMembers);
+router.get("/castMember/:name", controller.getOneCastMember);
+router.get("/random_team", controller.getRandomTeam);
 
 export = router;
