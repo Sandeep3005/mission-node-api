e05ea80b6998d5524c9bfd07287e01a1fa059253
Sandeep Chikhale <chikhale.sandeep.2002@gmail.com>
2021-11-08 12:16:58 +0530

    Update config

--- source/config/config.ts
+++ source/config/config.ts
@@ -1,17 +1,18 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 dotenv.config();
 
-const SERVER_HOSTNAME = process.env.SERVER_HOSTNAME || 'localhost';
-const SERVER_PORT = process.env.SERVER_PORT || 1337;
+console.log(process.env);
+const SERVER_HOSTNAME = process.env.HOST || "localhost";
+const SERVER_PORT = process.env.PORT || 1337;
 
 const SERVER = {
-    hostname: SERVER_HOSTNAME,
-    port: SERVER_PORT
+  hostname: SERVER_HOSTNAME,
+  port: SERVER_PORT,
 };
 
 const config = {
-    server: SERVER
+  server: SERVER,
 };
 
 export default config;
