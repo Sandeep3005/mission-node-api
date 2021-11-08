bc2b63f6685f9a6b2a7f114c064404f0cedc5fe7
Sandeep Chikhale <chikhale.sandeep.2002@gmail.com>
2021-11-08 15:09:56 +0530

    Update db-config.ts
    Remove console - 1

--- source/config/db-config.ts
+++ source/config/db-config.ts
@@ -8,7 +8,6 @@ const NAMESPACE = "DB_CONFIG";
 
 console.log("env ==== ",process.env.serviceAccount);
 console.log("env 3333 ==== ",process.env.CRED);
-console.log("env 3333 ==== ",process.env.PROD);
 
 admin.initializeApp({
   credential: admin.credential.cert(JSON.parse(process.env.CRED)),
